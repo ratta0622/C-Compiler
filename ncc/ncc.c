5ce91156068a7bf00cae1a6afac41571a5dca645
ratta0622 <ratta0622@gmail.com>
2024-10-25 10:33:33 +0000

    implement addition and subtraction of integers

--- ncc/ncc.c
+++ ncc/ncc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
     if(argc != 2){
@@ -7,11 +8,26 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    char *inp = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
-    printf("    ret \n");
 
+    printf("    mov rax, %ld\n", strtol(inp, &inp, 10));
+
+    while(*inp != '\0'){
+        if(*inp == '+'){
+            ++inp;
+            printf("    add rax, %ld\n", strtol(inp, &inp, 10));
+        }else if(*inp =='-'){
+            ++inp;
+            printf("    sub rax, %ld\n", strtol(inp, &inp, 10));
+        }else{
+            fprintf(stderr, "unexpected input character: %c\n",*inp);
+            exit(1);
+        }
+    }
+printf("    ret\n");
     return 0;
 }
